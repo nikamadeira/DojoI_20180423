ef6b2d316657d02b2a2ef480e62a7a9d07950535
nikamadeira <anac.borri@gmail.com>
2018-05-01 12:12:32 -0300

    Primeira carga do projeto

---
+++ features/Cadastrar_empregado.feature
@@ -0,0 +1,12 @@
+#language: pt
+#utf-8
+
+Funcionalidade: Cadastrar empregado	
+	Eu como analista
+	quero fazer um teste
+	para cadastrar empregados
+
+Cenário: Manter novo empregado
+	Dado que eu esteja no site da OrangeHRM
+	Quando eu acessar a tela de add employee
+	Então um novo empregado será cadastrado
---
+++ features/step_definitions/ManterUsuarios_steps.rb
@@ -0,0 +1,17 @@
+Dado("que eu esteja no site da OrangeHRM") do
+  visit 'http://opensource.demo.orangehrmlive.com/'
+  fill_in "txtUsername", :with => 'admin'
+  fill_in "txtPassword", :with => 'admin'
+  click_button id="btnLogin"
+end
+
+Quando("eu acessar a tela de add employee") do
+  click_link id="menu_pim_viewPimModule"
+  click_link id="menu_pim_addEmployee"
+end
+
+Então("um novo empregado será cadastrado") do
+  fill_in "firstName", :with => 'Ana'
+  fill_in "middleName", :with => 'Cláudia'
+  fill_in "lastName", :with => 'Menezes'
+end
